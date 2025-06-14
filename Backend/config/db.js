6b8187cae7e34164acb66f78306ad4974e52013a
Rishabh Tomar <rishabhtomar9999@gmail.com>
2025-06-14 20:44:04 +0530

    db connev

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
     process.exit(1);
   }
 };
