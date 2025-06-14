884a40b500961aae4540c97a92e23155dc928be7
Rishabh Tomar <rishabhtomar9999@gmail.com>
2025-06-14 20:25:00 +0530

    a

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
 import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <AuthProvider>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </AuthProvider>
 );
